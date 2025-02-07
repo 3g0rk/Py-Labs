aff3fdd668b700cc6fe2647665bd6708e5b123cd
Egor Kobeliatskii <94761567+3g0rk@users.noreply.github.com>
2025-02-07 19:05:01 +0300

    Add files via upload
    третья попытка сдачи 1 лабораторной

---
+++ 2 Modul/Lab 1/LAB1_Task2_Kobeliatskii.py
@@ -0,0 +1,23 @@
+from LAB1_Task1_Kobeliatskii import Library, Worker, Student
+
+if __name__ == "__main__":
+    library = Library("Городская библиотека", 2)
+    student = Student("Анна", 10, 3.5)
+    worker = Worker("Иван", "Менеджер", 50000)
+
+    try:
+        library.add_book("Книга 1")
+        library.add_book("Книга 2")
+        library.add_book("Книга 3")
+    except ValueError:
+        print("Ошибка: библиотека заполнена")
+
+    try:
+        student.update_gpa(6.0)
+    except ValueError:
+        print("Ошибка: неправильные данные")
+
+    try:
+        worker.update_salary(-1000)  
+    except ValueError:
+        print("Ошибка: неправильные данные")
